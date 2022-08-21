50dbde263bab86c6ae15b011b40cde607f8ea6e7
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2022-08-21 16:29:18 +0600

    All models added

---
+++ EMovieTicket/Data/MovieCategory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EMovieTicket.Data
+{
+    public enum MovieCategory
+    {
+        Action,
+        Comedy,
+        Drama,
+        Documentary,
+        Horror,
+        Fanstasy,
+        SciFi,
+        Animation
+    }
+}
